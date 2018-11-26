7e2e47f65a741aae360695940e1942a5904ba5dc
offiahchiaz <offiahchiaz@gmail.com>
2018-11-26 07:18:41 +0100

    Added Author model

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+// Set up mongoose connection
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://localhost:27017/Bookshelf';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
