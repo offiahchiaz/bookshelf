837196c873d5618ef236fdd88537549c1cf3c6b8
offiahchiaz <offiahchiaz@gmail.com>
2018-11-28 04:24:19 +0100

    Added author_list and genre_list file for displaying the lists of authors and genres respectively

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const Schema = mongoose.Schema;
 
@@ -28,7 +29,10 @@ AuthorSchema.virtual('name').get(function () {
 });
 // Virtual for author's lifespan
 AuthorSchema.virtual('lifespan').get(function () {
-    return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+    let birthYear = this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : 'Not Available';
+    let deathYear = this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '';
+    //return `${deathYear} - ${birthYear}`;  
+    return birthYear;
 });
 
 // Virtual for author's URL
--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const Schema = mongoose.Schema;
 
@@ -28,6 +29,10 @@ const BookInstanceSchema = new Schema({
 BookInstanceSchema.virtual('url').get(function () {
     return `/catalog/bookinstance/${this._id}`;
 });
+
+BookInstanceSchema.virtual('due_back_formatted').get(function () {
+    return moment(this.due_back).format('MMMM Do, YYYY');
+})
 
 // Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
