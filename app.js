b26284bd9932b9392e668b65ba78eea8b30e3500
offiahchiaz <offiahchiaz@gmail.com>
2018-11-30 06:44:46 +0100

    Minor change to app file

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const app = express();
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-// const mongoDB = 'mongodb://offiahchiaz:myogi2014@ds145128.mlab.com:45128/bookshelf';
 const mongoDB = 'mongodb://localhost:27017/bookshelf';
 mongoose.connect(mongoDB, {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
