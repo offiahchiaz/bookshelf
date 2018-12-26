b01412fdd5f8c13e9a66669b17158e066658aba8
Offiahchiaz <offiahchiaz@gmail.com>
2018-12-26 16:22:03 +0100

    Styling the UI

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const app = express();
 
 // Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost:27017/bookshelf';
+// mongodb://<dbuser>:<dbpassword>@ds145128.mlab.com:45128/bookshelf
+//const mongoDB = 'mongodb://localhost:27017/bookshelf';
+const mongoDB =  'mongodb://offiahchiaz:myogi2014@ds145128.mlab.com:45128/bookshelf';
 mongoose.connect(mongoDB, {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
-const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt-nodejs');
 
 const Schema = mongoose.Schema;
 
@@ -11,28 +10,39 @@ const UserSchema = new Schema({
         required: true,
         unique: true,
         minlength: 1,
-        validate: {
-            validator: validator.isEmail,
-            message: '{VALUE} is not a valid email'
-        }
+        lowercase: true
+
     },
     password: {
         type: String,
+        trim: true,
         required: true,
         minlength: 6
     },
-    tokens: [{
-        access: {
-            type: String,
-            required: true
-        },
-        token: {
-            type: String,
-            required: true
-        }
-    }]
+    address: {
+        type: String,
+        trim: true
+    }
 });
 
+// Hash the password before saving it to the database
+UserSchema.pre('save', function (next) {
+    const user = this;
+    if (!usser.isModified('password')) { return next();}
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) { return next(err);}
+        bcrypt.hash(user.password, salt, null, function (err, hash) {
+            if (err) { return next(err);}
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+// Compare password in the database and the one typed by the user
+UserSchema.methods.comparePassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+}; 
 
 // Export model
 module.exports = mongoose.model('User', UserSchema);
