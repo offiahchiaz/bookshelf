b364189e34c10b4c8e5fa3c20f3da4d65b6d74a0
offiahchiaz <offiahchiaz@gmail.com>
2021-08-01 22:45:23 +0100

    Added db connection string

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/cool', function(req, res, next) {
+  res.send('I am so cool');
+});
+
 module.exports = router;
