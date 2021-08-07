e7983f832cb9b0226385de049876ee50ae3a0a4e
offiahchiaz <offiahchiaz@gmail.com>
2021-08-07 14:26:26 +0100

    Added GET bookinstance_list method

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,8 +1,14 @@
 const BookInstance = require('../models/bookinstance');
 
 // Display list of all BookInstances.
-exports.bookinstance_list = (req, res) => {
-    res.send('NOT IMPLEMENTED: BookInstance list');
+exports.bookinstance_list = (req, res, next) => {
+    BookInstance.find()
+        .populate('book')
+        .exec((err, list_bookinstances) => {
+        if (err) { return next(err); }
+        // Successful, so render
+        res.render('catalog/bookinstance_list', { title: 'Book Instance List', list_bookinstances });
+    });
 };
 
 // Display detail page for a specific BookInstance.
