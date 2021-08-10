c98a019c5ccbf98e246a06bac93355e8330131c7
offiahchiaz <offiahchiaz@gmail.com>
2021-08-10 01:03:16 +0100

    Added GET /catalog/author/:id for displaying autho details

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,7 @@
+const async = require('async');
+
 const Author = require('../models/author');
+const Book = require('../models/book');
 
 // Display list of all Authors.
 exports.author_list = (req, res, next) => {
@@ -12,8 +15,26 @@ exports.author_list = (req, res, next) => {
 };
 
 // Display detail page for a specific Author.
-exports.author_detail = (req, res) => {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+exports.author_detail = (req, res, next) => {
+    async.parallel({
+        author: (callback) => {
+            Author.findById(req.params.id)
+                .exec(callback);
+        },
+        authors_books: (callback) => {
+            Book.find({'author': req.params.id}, 'title summary')
+                .exec(callback);
+        }
+    }, (err, results) => {
+        if (err) { return next(err); }
+        if (results.author == null) {
+            let err = new Error('Author not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render.
+        res.render('catalog/author_detail', { title: results.author.name, author: results.author, author_books: results.authors_books });
+    });
 };
 
 // Display Author create form on GET.
